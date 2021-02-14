73802aba964a606144d8c3c7ae4e1fe2194b293c
ZerpStvn <stevefelizardo4@gmail.com>
2021-02-14 12:49:18 +0800

    update

--- TravelCatalog/lib/Homepage.dart
+++ TravelCatalog/lib/Homepage.dart
@@ -11,14 +11,13 @@ class _HomepageState extends State<Homepage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
-          height: 300,
-          width: 500,
-          decoration: BoxDecoration(
-            color: Colors.lightBlueAccent,
-            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50)),
-            image:,
-          ),
-          ),
+        height: 300,
+        width: 500,
+        decoration: BoxDecoration(
+          color: Colors.lightBlueAccent,
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50)),
+        ),
+      ),
     );
   }
 }
