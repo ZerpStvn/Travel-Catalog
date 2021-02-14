e8bcaa1c87477b100ea12b3a4bd765aeca0f0b77
ZerpStvn <stevefelizardo4@gmail.com>
2021-02-14 12:43:54 +0800

    update

---
+++ TravelCatalog/lib/Homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+          height: 300,
+          width: 500,
+          decoration: BoxDecoration(
+            color: Colors.lightBlueAccent,
+            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50)),
+            image:,
+          ),
+          ),
+    );
+  }
+}
